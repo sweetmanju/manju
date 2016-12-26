e11547333b0009efc2948ca826445acfd84a47c0
sweetmanju <sweetmanju110@gmail.com>
2016-12-26 00:18:47 -0800

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int year;
+    scanf("\n %d",&year);
+    if(year%4==0)
+        printf("\n it is a leap year");
+    else
+        printf("\n it is not a leap year");
+}
+

530593c7dad6e44d2db5f2403e81522a26723b04
sweetmanju <sweetmanju110@gmail.com>
2016-12-26 00:23:52 -0800

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,11 @@
+#include<stdio.h>conio.h>
+void main()
+{
+    int n=1,sum=0;
+    while(n<21)
+    {
+        sum+=n;
+        n++;
+    }
+        printf("\n the sum of natural num is:%d",sum);
+}

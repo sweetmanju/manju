b1018edf70c3c56b29003e407b9b1c27742fbeff
sweetmanju <sweetmanju110@gmail.com>
2016-12-25 22:33:44 -0800

    Add files via upload

---
+++ positivenegative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int i;
+ scanf("\n %d",&i);
+ if(i>0)
+ {
+         printf("\n it is a positive number");
+ }
+ else if(i<0)
+ {
+        printf("\n it is a negative number");
+ }
+ else{
+    printf("\n it is zero");
+}
+}

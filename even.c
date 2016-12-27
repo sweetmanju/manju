50d1389578a6145c13d2baf1ac32254e8dbf7edf
sweetmanju <sweetmanju110@gmail.com>
2016-12-27 01:45:39 -0800

    Add files via upload

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int i,j,even;
+    printf("\n the limit is :",i,j);
+    scanf("\n %d %d:",&i,&j);
+    for(i;i<=j;i++)
+          {
+
+              if(i%2==0)
+                printf("\n %d is even number",i);
+          }
+
+}
